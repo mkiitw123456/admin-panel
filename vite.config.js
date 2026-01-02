be1ab456ba17e49cd5e1230ab1590debd70f0919
Jimmy <jimmylai.isynreal@gmail.com>
2026-01-02 18:06:16 +0800

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 注意：這裡必須是 '/您的GitHub儲存庫名稱/'，前後都要有斜線
-  base: '/admin-panel/', 
+  // Vercel 部署請使用 '/' (根目錄)，或者直接刪除這一行也可以
+  base: '/', 
 })
